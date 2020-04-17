38552f5ce89860b062bde1326940cdd6bd114bd5
BrunoGSA <brunogsa@live.com>
2020-04-17 20:49:53 -0300

    Compilations fixes and imgui output dir changed

--- vendor/build-imgui-windows.bat
+++ vendor/build-imgui-windows.bat
@@ -1,18 +1,19 @@
 @echo off 
 
 SET BUILD_DIR=imgui\build
-SET BUILD_TYPE=Release
+SET BUILD_TYPE=Debug
 SET SHARED_LIB=OFF
 
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-cmake .. -G "Visual Studio 15 2017 Win64"     ^
+cmake .. -G "Visual Studio 15 2017"           ^
 	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
 	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%    ^
 	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
-	-DOPERATING_SYSTEM:STRING=WINDOWS
+	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
+	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86\
 
 cmake --build . --config %BUILD_TYPE%
 
