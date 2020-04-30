dddcc5dfe341f5434cdaaf6ea13764a06e44b247
Bruno Gallego <brunogsa@live.com>
2020-04-30 16:28:18 -0300

    Folder output updated

--- vendor/build-glfw-linux.sh
+++ vendor/build-glfw-linux.sh
@@ -8,14 +8,29 @@ export SHARED_LIB=OFF
 export OUTPUT_DIR_32=../../lib/x86
 export OUTPUT_DIR_64=../../lib/x86_64
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
+create_dir()
+{
+	if [ ! -d "$1" ]; then
+		mkdir -p "$1"
+	fi
+}
+clear_build_dir()
+{
+	if [ -d $BUILD_DIR ]; then
+		rm -rf $BUILD_DIR
+	fi	  
+}
+make_build_dir()
+{
+	clear_build_dir
+	create_dir $BUILD_DIR
+}
 
-mkdir $BUILD_DIR
+
+make_build_dir
 cd $BUILD_DIR
 
-cmake .. -G "Unix Makefiles"                  \
+cmake .. -G "Unix Makefiles"                      \
 	-DGLFW_BUILD_EXAMPLES:BOOL=OFF            \
 	-DGLFW_BUILD_TESTS:BOOL=OFF               \
 	-DGLFW_BUILD_DOCS:BOOL=OFF                \
@@ -26,20 +41,18 @@ cmake .. -G "Unix Makefiles"                  \
 
 cmake --build . --config $BUILD_TYPE
 
+create_dir $OUTPUT_DIR_32/$BUILD_TYPE/
+
 if [ "$SHARED_LIB" == "OFF" ]; then
-	cp src/libglfw3.a   $OUTPUT_DIR_32
+	cp src/libglfw3.a   $OUTPUT_DIR_32/$BUILD_TYPE/
 else
-	cp src/libglfw3.so  $OUTPUT_DIR_32
+	cp src/libglfw3.so  $OUTPUT_DIR_32/$BUILD_TYPE/
 fi
+
 
 cd ../../
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
-
-
-mkdir $BUILD_DIR
+make_build_dir
 cd $BUILD_DIR
 
 cmake .. -G "Unix Makefiles"                  \
@@ -53,14 +66,14 @@ cmake .. -G "Unix Makefiles"                  \
 
 cmake --build . --config $BUILD_TYPE
 
+create_dir $OUTPUT_DIR_64/$BUILD_TYPE/
+
 if [ "$SHARED_LIB" == "OFF" ]; then
-	cp src/libglfw3.a   $OUTPUT_DIR_64
+	cp src/libglfw3.a   $OUTPUT_DIR_64/$BUILD_TYPE/
 else
-	cp src/libglfw3.so  $OUTPUT_DIR_64
+	cp src/libglfw3.so  $OUTPUT_DIR_64/$BUILD_TYPE/
 fi
 
 cd ../../
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
+clear_build_dir
--- vendor/build-googletest-linux.sh
+++ vendor/build-googletest-linux.sh
@@ -3,51 +3,63 @@
 set echo off
 
 export BUILD_DIR=googletest/build
+export BUILD_TYPE=Release
 export OUTPUT_DIR_32=lib/x86
 export OUTPUT_DIR_64=lib/x86_64
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
+create_dir()
+{
+	if [ ! -d "$1" ]; then
+		mkdir -p "$1"
+	fi
+}
+clear_build_dir()
+{
+	if [ -d $BUILD_DIR ]; then
+		rm -rf $BUILD_DIR
+	fi	  
+}
+make_build_dir()
+{
+	clear_build_dir
+	create_dir $BUILD_DIR
+}
 
-mkdir $BUILD_DIR
+
+make_build_dir
 cd $BUILD_DIR
 
-cmake .. -G "Unix Makefiles"                             \
-	-DBUILD_SHARED_LIBS:BOOL=OFF            \
-	-DCMAKE_BUILD_TYPE:STRING=Release    \
-	-DCMAKE_ENABLE_EXPORTS:BOOL=ON    \
-	-DCMAKE_CXX_FLAGS="-fPIC"                    \
+cmake .. -G "Unix Makefiles"                  \
+	-DBUILD_SHARED_LIBS:BOOL=OFF          \
+	-DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE \
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON        \
+	-DCMAKE_CXX_FLAGS="-fPIC"             \
 	-DCMAKE_C_FLAGS:STRING="-m32"             
 
 cmake --build . --config Release
 
 cd ../../
 
-cp $BUILD_DIR/lib/libgtest.a            $OUTPUT_DIR_32 
-cp $BUILD_DIR/lib/libgtest_main.a   $OUTPUT_DIR_32
+create_dir $OUTPUT_DIR_32/$BUILD_TYPE/
+cp -f $BUILD_DIR/lib/libgtest.a        $OUTPUT_DIR_32/$BUILD_TYPE/
+cp -f $BUILD_DIR/lib/libgtest_main.a   $OUTPUT_DIR_32/$BUILD_TYPE/
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
-
-mkdir $BUILD_DIR
+make_build_dir
 cd $BUILD_DIR
 
-cmake .. -G "Unix Makefiles"                             \
-	-DBUILD_SHARED_LIBS:BOOL=OFF            \
-	-DCMAKE_BUILD_TYPE:STRING=Release    \
-	-DCMAKE_ENABLE_EXPORTS:BOOL=ON    \
-	-DCMAKE_CXX_FLAGS="-fPIC"                    \
+cmake .. -G "Unix Makefiles"                  \
+	-DBUILD_SHARED_LIBS:BOOL=OFF          \
+	-DCMAKE_BUILD_TYPE:STRING=$BUILD_TYPE \
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON        \
+	-DCMAKE_CXX_FLAGS="-fPIC"             \
 	-DCMAKE_C_FLAGS:STRING="-m64"             
 
 cmake --build . --config Release
 
 cd ../../
 
-cp $BUILD_DIR/lib/libgtest.a            $OUTPUT_DIR_64
-cp $BUILD_DIR/lib/libgtest_main.a   $OUTPUT_DIR_64
+create_dir $OUTPUT_DIR_64/$BUILD_TYPE/
+cp -f $BUILD_DIR/lib/libgtest.a        $OUTPUT_DIR_64/$BUILD_TYPE/
+cp -f $BUILD_DIR/lib/libgtest_main.a   $OUTPUT_DIR_64/$BUILD_TYPE/
 
-if [ -d $BUILD_DIR ]; then
-	rm -rf $BUILD_DIR
-fi
+clear_build_dir
