63919ac392ff16fffae5fac3dcac33b3d4c164fb
BrunoGSA <bruno.amaral@trt1.jus.br>
2021-05-22 17:23:41 -0300

    x64 support

--- vendor/build-glfw-windows.bat
+++ vendor/build-glfw-windows.bat
@@ -21,20 +21,20 @@ cmake .. -G "Visual Studio 15 2017"                                ^
 	-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"         ^
 	-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"
 
-cmake --build . --config Debug
+cmake --build . --config %BUILD_TYPE%
 
-if EXIST "%OUTPUT_DIR%\Debug\glfw3.dll" DEL /F "%OUTPUT_DIR%\Debug\glfw3.dll"
-if EXIST "%OUTPUT_DIR%\Debug\glfw3.ilk" DEL /F "%OUTPUT_DIR%\Debug\glfw3.ilk"
-if EXIST "%OUTPUT_DIR%\Debug\glfw3.pdb" DEL /F "%OUTPUT_DIR%\Debug\glfw3.pdb"
-if EXIST "%OUTPUT_DIR%\Debug\glfw.pdb"  DEL /F "%OUTPUT_DIR%\Debug\glfw.pdb"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.pdb" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.pdb"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw.pdb"  DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw.pdb"
 
-copy "src\glfw.dir\Debug\glfw.pdb"    "%OUTPUT_DIR%\Debug"
+copy "src\glfw.dir\%BUILD_TYPE%\glfw.pdb"    "%OUTPUT_DIR%\%BUILD_TYPE%"
 
 cd ..\..\
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 
 
-
+SET BUILD_TYPE=Release
 
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
@@ -49,10 +49,68 @@ cmake .. -G "Visual Studio 15 2017"                                ^
 	-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"         ^
 	-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"
 
-cmake --build . --config Release
+cmake --build . --config %BUILD_TYPE%
 
-if EXIST "%OUTPUT_DIR%\Release\glfw3.dll" DEL /F "%OUTPUT_DIR%\Release\glfw3.dll"
-if EXIST "%OUTPUT_DIR%\Release\glfw3.ilk" DEL /F "%OUTPUT_DIR%\Release\glfw3.ilk"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk"
+
+cd ..\..\
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
+
+
+
+
+SET ARCH_BITS=64
+SET BUILD_DIR=glfw\build
+SET BUILD_TYPE=Debug
+SET SHARED_LIB=OFF
+SET OUTPUT_DIR=%~dp0%lib\x86_64
+
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake .. -G "Visual Studio 15 2017 Win64"                          ^
+	-DGLFW_BUILD_EXAMPLES:BOOL=OFF                                 ^
+	-DGLFW_BUILD_TESTS:BOOL=OFF                                    ^
+	-DGLFW_BUILD_DOCS:BOOL=OFF                                     ^
+	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%                          ^
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON                                 ^
+	-DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=ON                         ^
+	-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"         ^
+	-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"
+
+cmake --build . --config %BUILD_TYPE%
+
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.pdb" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.pdb"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw.pdb"  DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw.pdb"
+
+copy "src\glfw.dir\%BUILD_TYPE%\glfw.pdb"    "%OUTPUT_DIR%\%BUILD_TYPE%"
+
+cd ..\..\
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
+
+
+SET BUILD_TYPE=Release
+
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake .. -G "Visual Studio 15 2017 Win64"                          ^
+	-DGLFW_BUILD_EXAMPLES:BOOL=OFF                                 ^
+	-DGLFW_BUILD_TESTS:BOOL=OFF                                    ^
+	-DGLFW_BUILD_DOCS:BOOL=OFF                                     ^
+	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%                          ^
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON                                 ^
+	-DUSE_MSVC_RUNTIME_LIBRARY_DLL:BOOL=ON                         ^
+	-DCMAKE_LIBRARY_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"         ^
+	-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY:STRING="%OUTPUT_DIR%"
+
+cmake --build . --config %BUILD_TYPE%
+
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.dll"
+if EXIST "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk" DEL /F "%OUTPUT_DIR%\%BUILD_TYPE%\glfw3.ilk"
 
 cd ..\..\
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
--- vendor/build-imgui-windows.bat
+++ vendor/build-imgui-windows.bat
@@ -1,8 +1,8 @@
 @echo off 
 
 SET BUILD_DIR=imgui\build
-SET BUILD_TYPE=Debug
 SET SHARED_LIB=OFF
+SET BUILD_TYPE=Debug
 
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 mkdir %BUILD_DIR%
@@ -10,7 +10,7 @@ cd %BUILD_DIR%
 
 cmake .. -G "Visual Studio 15 2017"           ^
 	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
-	-DBUILD_TYPE:STRING=%BUILD_TYPE%          ^
+	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%          ^
 	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
 	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
 	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86\
@@ -20,14 +20,41 @@ cmake --build . --config %BUILD_TYPE%
 cd ..\..\
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 
-cd include
-if exist imgui ( rmdir /s/q imgui )
-mkdir imgui
-copy "..\imgui\*.h"   "imgui\"
 
-cd ..\
+SET BUILD_TYPE=Release
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake .. -G "Visual Studio 15 2017"           ^
+	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
+	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%          ^
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
+	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
+	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86\
+
+cmake --build . --config %BUILD_TYPE%
+
+cd ..\..\
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 
 
+
+SET BUILD_TYPE=Debug
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
+mkdir %BUILD_DIR%
+cd %BUILD_DIR%
+
+cmake .. -G "Visual Studio 15 2017 Win64"           ^
+	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
+	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%          ^
+	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
+	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
+	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86_64\
+
+cmake --build . --config %BUILD_TYPE%
+
+cd ..\..\
+if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
 
 
 SET BUILD_TYPE=Release
@@ -35,17 +62,19 @@ SET BUILD_TYPE=Release
 mkdir %BUILD_DIR%
 cd %BUILD_DIR%
 
-cmake .. -G "Visual Studio 15 2017"           ^
+cmake .. -G "Visual Studio 15 2017 Win64"           ^
 	-DBUILD_SHARED_LIBS:BOOL=%SHARED_LIB%     ^
-	-DBUILD_TYPE:STRING=%BUILD_TYPE%          ^
+	-DCMAKE_BUILD_TYPE:STRING=%BUILD_TYPE%          ^
 	-DCMAKE_ENABLE_EXPORTS:BOOL=ON            ^
 	-DOPERATING_SYSTEM:STRING=WINDOWS         ^
-	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86\
+	-DOUTPUT_LIB_DIR:STRING=..\..\lib\x86_64\
 
 cmake --build . --config %BUILD_TYPE%
 
 cd ..\..\
 if exist %BUILD_DIR% ( rmdir /s/q %BUILD_DIR% )
+
+
 
 cd include
 if exist imgui ( rmdir /s/q imgui )
