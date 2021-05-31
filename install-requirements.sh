25cbcd93a437de93374176cdbc987177266b2fd5
BrunoGSA <bruno.amaral@trt1.jus.br>
2021-05-31 09:44:46 -0300

    x86 dependencies removed

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
-
 # Linux requirements
 
 set echo off
 
-sudo dpkg --add-architecture i386
+sudo apt-get update
 
-sudo apt-get dist-upgrade
+sudo apt-get install -y gcc-multilib g++-multilib libx11-dev build-essential cmake
 
-sudo apt-get install -y ia32-libs
+sudo apt-get install libgl1-mesa-glx:amd64 libgl1-mesa-dri:amd64
 
-sudo apt-get install -y gcc-multilib g++-multilib cmake libx11-dev libx11-dev:i386 libxext-dev:i386 libxmu-dev:i386 libxi-dev:i386
-
-sudo apt-get install -y build-essential build-essential:i386
-
-sudo apt-get install libgl1-mesa-dev:i386 libgl1-mesa-glx:i386 libgl1-mesa-glx:amd64 libgl1-mesa-dri:i386 libgl1-mesa-dri:amd64 xserver-xorg-core
-
-sudo apt-get install -y xorg-dev
-
-sudo apt-get install -y mesa-utils mesa-utils:i386 mesa-common-dev mesa-common-dev:i386
-
-sudo apt-get install -y libglew-dev
-
-sudo apt-get install -y libglfw3-dev
-
-sudo apt-get install -y qt5-default libqt5xmlpatterns5-dev
+sudo apt-get install -y xserver-xorg-core xorg-dev mesa-utils mesa-common-dev libglew-dev libglfw3-dev
